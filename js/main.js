0bdd0c2db90cfdbc829c4bb4db35d33d8074efcc
Indrjaith K L <mac91112@gmail.com>
2019-11-25 15:16:36 +0530

    25-11-2019

--- js/main.js
+++ js/main.js
@@ -1,23 +1,43 @@
 (function () {
-    function loadPopularMovies() {
+
+    function Movie(){
+        var searchInput = document.getElementById('search-input');
+        searchInput.addEventListener('input',function(event){
+            var searchText = event.target.value;
+            var availableMoviesRaw = localStorage.getItem('popular-movies');
+            if(availableMoviesRaw){
+                var availableMovies = JSON.parse(availableMoviesRaw).results;
+                var filteredMovies = availableMovies.filter(function(_movie){
+                    return(_movie.title.toLowerCase().indexOf(searchText.toLowerCase())>-1);
+                });
+                if(filteredMovies.length>0){
+                    document.getElementById('renderArea').innerHTML = "";
+                    this.movieListing(filteredMovies);
+
+                }
+            }
+        }.bind(this))
+    }
+
+    Movie.prototype.loadPopularMovies = function(){
         const xhr = new XMLHttpRequest(),
-            method = "GET",
-            url = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=06dcefc4c6268cb53b82f76560368636";
+        method = "GET",
+        url = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=06dcefc4c6268cb53b82f76560368636";
 
         xhr.open(method, url, true);
         xhr.onreadystatechange = function () {
-            debugger;
             if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
                 // console.log(xhr.responseText);
                 var rawResponseData = xhr.responseText;
+                localStorage.setItem('popular-movies',rawResponseData);
                 var jsonData = JSON.parse(rawResponseData);
-                movieListing(jsonData.results);
+                this.movieListing(jsonData.results);
             }
-        };
+        }.bind(this);
         xhr.send();
-    }
+    } 
 
-    function movieListing(movies){
+    Movie.prototype.movieListing = function(movies){
         var totalMovies = movies.length;
         var renderArea = document.getElementById('renderArea');
         var row = document.createElement('div');
@@ -47,6 +67,7 @@
         }
     }
 
+    var movieInstance = new Movie();
+    movieInstance.loadPopularMovies();
 
-    loadPopularMovies();
 })();
